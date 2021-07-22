37bd7791e3940bc735ae441fbcd6ecb01f475cf0
Johnson Tinuoye <johnsontinuoye@gmail.com>
2021-07-22 13:05:26 +0100

    Fix bug with element container style

--- src/subcomponents/FBInput/fb-input.tsx
+++ src/subcomponents/FBInput/fb-input.tsx
@@ -1,39 +1,43 @@
-import { ChangeEvent } from 'react';
-import { FBErrorPositons, IFBInput } from '../../types';
+import { ChangeEvent } from "react";
+import { FBErrorPositons, IFBInput } from "../../types";
 type genericFBElementType = { onUpdate: (name: string, value: string) => void };
 
 const FBInput = ({
-    error,
-    label,
-    onUpdate,
-    onChange,
-    containerStyle = {},
-    elementStyle = {},
-    containerClass = '',
-    style = {},
-    className = '',
-    ...rest
+  error,
+  label,
+  onUpdate,
+  onChange,
+  containerStyle = {},
+  elementStyle = {},
+  containerClass = "",
+  style = {},
+  className = "",
+  ...rest
 }: IFBInput & genericFBElementType): JSX.Element => {
-    const onValueUpdate = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e);
-        onUpdate(e.target.name, e.target.value);
-    };
-    return (
-        <div
-            className={`${className} ${containerClass} ${error?.errorClass || ''}`}
-            style={Object.assign(containerStyle, error?.errorStyle || {})}
-        >
-            <label htmlFor={rest.id || rest.name}>{label}</label>
-            {error?.messagePosition === FBErrorPositons.TOP && <span>{error.message}</span>}
-            <input
-                id={rest.id || rest.name}
-                style={Object.assign(elementStyle, style)}
-                onChange={onValueUpdate}
-                {...rest}
-            />
-            {error?.messagePosition === FBErrorPositons.BOTTOM && <span>{error.message}</span>}
-        </div>
-    );
+  const onValueUpdate = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e);
+    onUpdate(e.target.name, e.target.value);
+  };
+  return (
+    <div
+      className={`${className} ${containerClass} ${error?.errorClass || ""}`}
+      style={{ ...containerStyle, ...(error?.errorStyle || {}), ...style }}
+    >
+      <label htmlFor={rest.id || rest.name}>{label}</label>
+      {error?.messagePosition === FBErrorPositons.TOP && (
+        <span>{error.message}</span>
+      )}
+      <input
+        id={rest.id || rest.name}
+        style={elementStyle}
+        onChange={onValueUpdate}
+        {...rest}
+      />
+      {error?.messagePosition === FBErrorPositons.BOTTOM && (
+        <span>{error.message}</span>
+      )}
+    </div>
+  );
 };
 
 export default FBInput;
--- src/subcomponents/FBTextArea/fb-text-area.tsx
+++ src/subcomponents/FBTextArea/fb-text-area.tsx
@@ -1,39 +1,43 @@
-import { ChangeEvent } from 'react';
-import { FBErrorPositons, IFBTextArea } from '../../types';
+import { ChangeEvent } from "react";
+import { FBErrorPositons, IFBTextArea } from "../../types";
 type genericFBElementType = { onUpdate: (name: string, value: string) => void };
 
 const FBTextArea = ({
-    error,
-    label,
-    onUpdate,
-    onChange,
-    containerStyle = {},
-    elementStyle = {},
-    containerClass = '',
-    style = {},
-    className = '',
-    ...rest
+  error,
+  label,
+  onUpdate,
+  onChange,
+  containerStyle = {},
+  elementStyle = {},
+  containerClass = "",
+  style = {},
+  className = "",
+  ...rest
 }: IFBTextArea & genericFBElementType): JSX.Element => {
-    const onValueUpdate = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        onChange && onChange(e);
-        onUpdate(e.target.name, e.target.value);
-    };
-    return (
-        <div
-            className={`${className} ${containerClass} ${error?.errorClass || ''}`}
-            style={Object.assign(containerStyle, error?.errorStyle || {})}
-        >
-            <label htmlFor={rest.id || rest.name}>{label}</label>
-            {error?.messagePosition === FBErrorPositons.TOP && <span>{error.message}</span>}
-            <textarea
-                id={rest.id || rest.name}
-                style={Object.assign(elementStyle, style)}
-                onChange={onValueUpdate}
-                {...rest}
-            />
-            {error?.messagePosition === FBErrorPositons.BOTTOM && <span>{error.message}</span>}
-        </div>
-    );
+  const onValueUpdate = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange && onChange(e);
+    onUpdate(e.target.name, e.target.value);
+  };
+  return (
+    <div
+      className={`${className} ${containerClass} ${error?.errorClass || ""}`}
+      style={{ ...containerStyle, ...(error?.errorStyle || {}), ...style }}
+    >
+      <label htmlFor={rest.id || rest.name}>{label}</label>
+      {error?.messagePosition === FBErrorPositons.TOP && (
+        <span>{error.message}</span>
+      )}
+      <textarea
+        id={rest.id || rest.name}
+        style={elementStyle}
+        onChange={onValueUpdate}
+        {...rest}
+      />
+      {error?.messagePosition === FBErrorPositons.BOTTOM && (
+        <span>{error.message}</span>
+      )}
+    </div>
+  );
 };
 
 export default FBTextArea;
